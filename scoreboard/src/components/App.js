cf4853c159b1f28f93e942b7a5d8224ce738aaac
Khaled Adad <khaledadad@lewisu.edu>
2021-06-12 09:22:43 -0500

    refactored by uplifting state of score to app

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header'
 import Player from './Player'
 
-class App extends React.Component {
+class App extends Component {
   state = {
     players: [
       {
         name: "Guil",
+        score: 0,
         id: 1
       },
       {
         name: "Treasure",
+        score: 0,
         id: 2
       },
       {
         name: "Ashley",
+        score: 0,
         id: 3
       },
       {
         name: "James",
+        score: 0,
         id: 4
       }
     ]
   };
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+    // console.log('index: ' + index, 'delta: ' + delta)
+  }
 
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
@@ -41,11 +52,14 @@ class App extends React.Component {
         />
   
         {/* Players list */}
-        {this.state.players.map( player =>
+        {this.state.players.map( (player, index) =>
           <Player 
             name={player.name}
+            score={player.score}
             id={player.id}
             key={player.id.toString()} 
+            index={index}
+            scoreChange={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}           
           />
         )}
