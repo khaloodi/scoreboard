aac66a9ed8b15be8f76163a5f844e6e98b1b4f0f
Khaled Adad <khaledadad@lewisu.edu>
2021-06-13 10:44:12 -0500

    use refs instead of controlled components in addPF

--- scoreboard/src/components/AddPlayerForm.js
+++ scoreboard/src/components/AddPlayerForm.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react'
 
 class AddPlayerForm extends Component {
 
-  state = {
-    value: ''
-  }
+  // state = {
+  //   value: ''
+  // }
 
-  handleValueChange = (e) => {
-    this.setState({value: e.target.value})
-  }
+  // using refs, the DOM doesn't update after every keystroke
+  playerInput = React.createRef()
+
+  // handleValueChange = (e) => {
+  //   this.setState({value: e.target.value})
+  // }
 
   handleSubmit = (e) => {
     e.preventDefault()
-    this.props.addPlayer(this.state.value)
-    this.setState({value: ''})
+    // this.props.addPlayer(this.state.value)
+    this.props.addPlayer(this.playerInput.current.value)
+    // this.setState({value: ''})
+    e.currentTarget.reset() //resets form submission after submit, added this b/c using refs instead of state
   }
 
   render() {
-    console.log(this.state.value)
+    // console.log(this.state.value)
+    console.log(this.playerInput)
     return (
       <form onSubmit={this.handleSubmit}>
         <input 
           type="text" 
-          value={this.state.value}
-          onChange={this.handleValueChange}
+          ref = {this.playerInput}
+          // value={this.state.value}
+          // onChange={this.handleValueChange}
           placeholder="Enter a player's name"
         />
         <input 
