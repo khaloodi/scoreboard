8db5881d41505d81dfcbd70e1674392ab10a6f1d
Khaled Adad <khaledadad@lewisu.edu>
2021-06-12 20:23:41 -0500

    created timer functionality with tick + hook

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -2,13 +2,37 @@ import React, {Component} from 'react'
 
 class Stopwatch extends Component {
   state = {
-    isRunning: false
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
+  }
+
+  componentDidMount() {
+    console.log('The stopwatch mounted!')
+    this.intervalID = setInterval(() => this.tick(), 100) // time delay of 100 = 1/10th of a second
+  }
+
+  tick = () => {
+    if (this.state.isRunning) {
+      const now = Date.now()
+      this.setState(prevState => ({
+        previousTime: now,
+        elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+      }))
+    }
   }
 
   handleStopwatch = () => {
-    this.setState({
-      isRunning: !this.state.isRunning
-    })
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }))
+
+    // the timer only changes the time if timer is running or is true
+    if(!this.state.isRunning) {
+      this.setState({
+        previousTime: Date.now()
+      })
+    }
   }
   render() {
 
